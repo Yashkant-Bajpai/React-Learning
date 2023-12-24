efbf1e8fea95f3ebd8958fd04cf1c41c5ae307a4
Yashkant-Bajpai <yashkant.bajpai_cs20@gla.ac.in>
2023-12-24 17:25:10 +0530

    final commit

---
+++ yash-project/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+
+    return (
+      <div className="App">
+        <p> This is a React Component</p>
+      </div>
+    );
+  }
+
+export default App;
