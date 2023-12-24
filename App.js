501a101b6de8c14e1512d8249add339518275d4e
Yashkant-Bajpai <yashkant.bajpai_cs20@gla.ac.in>
2023-12-24 14:19:04 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,33 @@
+const parent = React.createElement(
+    "div",
+    {id : "parent"},
+   [ React.createElement(
+    "div",
+    {id : "child1"},
+   [ React.createElement(
+    "h1",
+    {},
+    "I'm H1 Tag of child 1"
+),
+React.createElement(
+    "h2",
+    {},
+    "I'm H2 Tag of child 1"
+)]),
+React.createElement(
+    "div",
+    {id : "child2"},
+   [ React.createElement(
+    "h1",
+    {},
+    "I'm H1 Tag of child 2"
+),
+React.createElement(
+    "h2",
+    {},
+    "I'm H2 Tag of child 2"
+)])]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
